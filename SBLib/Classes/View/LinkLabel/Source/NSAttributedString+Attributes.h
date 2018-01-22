4a8668dc25f52912c27bcb727b681ff278cd3d99
zhangsongtao <songtao@eastmoney.com>
2018-01-22 16:04:31 +0800

    kOHEmoitAttributeName引用的地方再加上

--- SBLib/Classes/View/LinkLabel/Source/NSAttributedString+Attributes.h
+++ SBLib/Classes/View/LinkLabel/Source/NSAttributedString+Attributes.h
@@ -43,6 +43,9 @@
 #define MRC_AUTORELEASE(x) (x)
 #endif
 
+extern NSString* kOHLinkAttributeName;
+extern NSString* kOHEmoitAttributeName;
+
 //NSAttributedString扩展
 @interface NSAttributedString (OHCommodityConstructors)
 //初始化方法
